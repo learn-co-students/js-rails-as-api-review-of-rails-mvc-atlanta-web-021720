45ab31faa7cd16bdf23009833240542e2818a7f3
ChanPark <thechanmoon@gmail.com>
2020-04-04 06:49:12 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,10 @@
 # Add code from Readme
+# Rails.application.routes.draw do
+#     get '/birds' => 'birds#index'
+#   end
+class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+    render 'birds/index.html.erb'
+  end
+end
